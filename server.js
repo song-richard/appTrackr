eea262fbf26315c5a843ad762e9413cb1f24ac1f
song-richard <rsong1993@gmail.com>
2023-12-04 16:19:53 -0800

    Added initial structure

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8888;
 
+app.set('view engine', 'ejs');
 
+app.get('/home', (req, res) => {
+    res.render('home')
+})
 
 app.listen(`${PORT}`, (req, res) => {
-    console.log(`Listening on PORT: ${PORT}`)
+    console.log(`Listening on PORT: ${PORT}`);
 })
