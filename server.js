b45e5089c4594ef1685491c2a2f817ff1d891401
song-richard <rsong1993@gmail.com>
2023-12-05 21:48:53 -0800

    Added mongoose. Verified connection with MongoDB

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8888;
 const mongoCred = require('./config/config');
+const mongoose = require('mongoose');
+
+connectToMongoDB();
 
 app.set('view engine', 'ejs');
-
-
 
 app.get('/home', (req, res) => {
     res.render('home')
@@ -14,3 +15,12 @@ app.get('/home', (req, res) => {
 app.listen(`${PORT}`, (req, res) => {
     console.log(`Listening on PORT: ${PORT}`);
 })
+
+async function connectToMongoDB() {
+    try {
+        await mongoose.connect(mongoCred);
+        console.log("Connected to MongoDB!")
+    } catch (err) {
+        console.error(err)
+    }
+}
