a97296b0b65ab15878e5a307dcd4e7fec6a95738
song-richard <rsong1993@gmail.com>
2023-12-05 22:24:33 -0800

    Added resetFields() to reset input fields after data submission

--- public/addFunc.js
+++ public/addFunc.js
@@ -1,4 +1,4 @@
-console.log("adddFunc.js loaded!")
+console.log("adddFunc.js loaded!");
 
 const addBtn = document.querySelector('#addBtn');
 
@@ -9,17 +9,29 @@ function getFormData() {
     const statusVal = document.querySelector('#status').value;
     const notesVal = document.querySelector('#notes').value;
 
-    return {
-        jobTitleVal: jobTitleVal,
-        compamyVal: compamyVal,
-        applicationDateVal: applicationDateVal,
-        statusVal: statusVal,
-        notesVal: notesVal
+    try {
+        return {
+            jobTitleVal: jobTitleVal,
+            compamyVal: compamyVal,
+            applicationDateVal: applicationDateVal,
+            statusVal: statusVal,
+            notesVal: notesVal
+        };
+    } finally {
+        resetFields();
     };
-}
+
+    function resetFields() {
+        document.querySelector('#jobTitle').value = '';
+        document.querySelector('#company').value = '';
+        document.querySelector('#applicationDate').value = '';
+        document.querySelector('#status').value = '';
+        document.querySelector('#notes').value = '';
+    };
+};
 
 document.addEventListener('DOMContentLoaded', function() {
-    addBtn.addEventListener('click', addApp)
+    addBtn.addEventListener('click', addApp);
 })
 
 async function addApp() {
@@ -27,9 +39,9 @@ async function addApp() {
         const formData = getFormData();
         console.log(formData);
 
-        await axios.post('/add-app', { formData })
+        // await axios.post('/add-app', { formData })
 
     } catch (err) {
-        console.error(err)
-    }
-}
+        console.error(err);
+    };
+};
