42dfb51a3b29715a14cbb04c686ec6022773cb53
Richard Song <rsong1993@gmail.com>
2023-12-08 15:10:37 -0800

    Added listApps script to ejs

--- public/listApps.js
+++ public/listApps.js
@@ -1,9 +1,14 @@
-const listUL = document.querySelector('appListUL');
 
 async function retrieveApps() {
+    const listUL = document.querySelector('appListUL');
     try {
         const response = axios.get('/get-app')
-        
+        const responseData = response.data
+        responseData.forEach(app => {
+            let newLi = document.createElement('li');
+            newLi.textContent = app.textContent
+            listUL.appendChild(newLi);
+        })
     } catch (err) {
         console.log(err)
     }
