f747c73b3c219e1dee5cd8e8c50f0224fcd22841
Richard Song <rsong1993@gmail.com>
2023-12-08 15:08:26 -0800

    Added listApps for data retrieveal to generate list of applications

--- public/listApps.js
+++ public/listApps.js
@@ -1,2 +1,10 @@
 const listUL = document.querySelector('appListUL');
 
+async function retrieveApps() {
+    try {
+        const response = axios.get('/get-app')
+        
+    } catch (err) {
+        console.log(err)
+    }
+}
