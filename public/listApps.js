eef3ea0dcd6da86913e1e56451e56c17f1479858
Richard Song <rsong1993@gmail.com>
2023-12-08 15:11:38 -0800

    Added retrieveApps func call to handle retrieval of apps. Will generate a list based on data retrieved

--- public/listApps.js
+++ public/listApps.js
@@ -2,14 +2,14 @@
 async function retrieveApps() {
     const listUL = document.querySelector('appListUL');
     try {
-        const response = axios.get('/get-app')
-        const responseData = response.data
+        const response = axios.get('/get-app');
+        const responseData = response.data;
         responseData.forEach(app => {
             let newLi = document.createElement('li');
-            newLi.textContent = app.textContent
+            newLi.textContent = app.textContent;
             listUL.appendChild(newLi);
-        })
+        });
     } catch (err) {
         console.log(err)
-    }
-}
+    };
+};
