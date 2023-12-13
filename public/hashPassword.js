f0d5c7ba9c81331ab7ea0f0f395ca558cca95c74
Richard Song <rsong1993@gmail.com>
2023-12-12 20:52:25 -0800

    Tested password hashing with register btn. Successfully hashed password on the server side

--- public/hashPassword.js
+++ public/hashPassword.js
@@ -1,29 +1,5 @@
-// const saltRounds = 10;
 
-
-
-// //Login Values
-// const loginEmailVal = document.querySelector('#loginEmail').value;
-// const loginPwVal = document.querySelector('#loginPassword').value;
-// const loginBtn = document.querySelector('#loginBtn');
-
-// async function hashPassword(plainPassword) {
-//     try {
-//         await bcrypt.hashSync(plainPassword, saltRounds);
-//     } catch (err) {
-//         console.error(err)
-//     }
-// }
-
-// function handleLogin() {
-//     const plainPassword = document.getElementById('password').value;
-
-//     const hashedPassword = hashPassword(plainPassword);
-
-//     console.log('Hashed Password:', hashedPassword);
-
-// }
-
+//Testing Password Hashing
 registerBtn.addEventListener('click', async function () {
     //Register Values
     const email = document.querySelector('#registerEmail').value;
@@ -33,5 +9,5 @@ registerBtn.addEventListener('click', async function () {
         await axios.post('/register', { email, password });
     } catch (err) {
         console.error(err)
-    }
-})
+    };
+});
