094b271ce959ae2ec70e67655d54e81a142ff43f
Richard Song <rsong1993@gmail.com>
2023-12-14 14:27:57 -0800

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 // const mongoCred = require('./config/config');
 const mongoose = require('mongoose');
 const JobApplication = require('./models/appSchema');
