ce901ff8c8a55b98ecd8356320774ab7254cdadf
song-richard <rsong1993@gmail.com>
2023-12-15 11:35:21 -0800

    Added error handling modals for login page

--- public/hashPassword.js
+++ public/hashPassword.js
@@ -1,11 +1,18 @@
 //Testing Password Hashing
+const closeModalBtn = document.querySelector('#closeModalBtn');
+const closeSuccessModalBtn = document.querySelector('#closeSuccessModalBtn');
+const errorModal = document.querySelector('#errorModal');
+const successModal = document.querySelector('#successModal');
+
 registerBtn.addEventListener('click', async function () {
     //Register Values
     const email = document.querySelector('#registerEmail').value;
     const password = document.querySelector('#registerPassword').value;
     const registerBtn = document.querySelector('#registerBtn');
+    
     try {
         await axios.post('/register', { email, password });
+        showSuccessModal();
     } catch (err) {
         console.error(err);
     };
@@ -26,5 +33,32 @@ loginBtn.addEventListener('click', async function () {
 
     } catch (err) {
         console.error(err);
+        showErrorModal();
     }
+});
+
+
+//Modal Functions
+closeModalBtn.addEventListener('click', function () {
+    hideErrorModal();
+});
+
+function showErrorModal() {
+    errorModal.classList.remove('hidden');
+}
+
+function hideErrorModal() {
+    errorModal.classList.add('hidden');
+}
+
+function showSuccessModal() {
+    successModal.classList.remove('hidden');
+}
+
+function hideSuccessModal() {
+    successModal.classList.add('hidden');
+}
+
+closeSuccessModalBtn.addEventListener('click', function () {
+    hideSuccessModal();
 });
